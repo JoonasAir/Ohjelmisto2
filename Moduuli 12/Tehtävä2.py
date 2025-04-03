3a38c6d7ca19a3a75dc6ad309ad9eea0459285e5
JoonasAir <joonaair@metropolia.fi>
2025-04-03 21:05:03 +0300

    up

--- Moduuli 12/Tehtävä2.py
+++ Moduuli 12/Tehtävä2.py
@@ -18,11 +18,10 @@ try:
     response = requests.get(url)
     if response.status_code == 200:
         data = response.json()
-    print(f"Säätila kaupungissa: {kaupunki}\n   {data['weather'][0]['description']}")
-    print(f"Lämpötila: {data ['main']['temp']} C")
+        print(f"Säätila kaupungissa: {kaupunki}\n   {data['weather'][0]['description']}")
+        print(f"Lämpötila: {data ['main']['temp']} C")
 except requests.exceptions.RequestException as e:
-    print("Hakua ei voitu suorittaa.")
-    
+    print("Hakua ei voitu suorittaa.")   
 except NameError:
     print(f"\nKirjoittamaasi kaupunkia {kaupunki} ei löytynyt.")
 

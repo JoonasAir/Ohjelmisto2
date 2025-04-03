ab8270503771b41623e7d7e3357235996b56141c
joonas <joonaair@metropolia.fi>
2025-04-03 14:04:46 +0300

    up

--- JS Moduuli 1/tehtävä10/tehtävä10.js
+++ JS Moduuli 1/tehtävä10/tehtävä10.js
@@ -1,20 +1,28 @@
 "use strict";
 
-let dicecount = +prompt("Give a dice amount")
+let dicecount = +prompt("Give a dice amount");
 
-let eyesum = +prompt("For what eye numbers you want to calculate probability?")
+let eyesum = +prompt("For what eye numbers you want to calculate probability?");
 
-let eyesumtotal = 0
+let eyesumtotal = 0;
+let mil = 1000000;
+let sum;
 
-for (let i = dicecount; i <= 1000000; i+= dicecount) {
+for (let i = 1; i <= mil; i++) {
+    sum = 0;
+
+    for (let j = 1; j <= dicecount; j++){
+
     let dice = Math.floor(Math.random() * 6) +1;
-    let dicesum = dice * dicecount
-    if (dicesum === eyesum){
-        eyesumtotal += 1
-    }
+    sum += dice;
+    } if (sum === eyesum) {
+        eyesumtotal += 1;
+}  
 
 }
-let probability = (eyesumtotal / 1000000 * 100).toFixed(2);
 
 
-document.querySelector("#answer").innerHTML = probability
+let probability = (eyesumtotal / mil * 100).toFixed(2);
+
+
+document.querySelector("#answer").innerHTML = probability;
--- JS Moduuli 1/tehtävä9/tehtävä9.js
+++ JS Moduuli 1/tehtävä9/tehtävä9.js
@@ -2,3 +2,20 @@
 
 let num = +prompt("Give me a number, i'll check is it a prime number or not.")
 
+if (num < 2) {
+    document.querySelector("#answer").innerHTML = "Given number " + num + " is not a prime number." 
+} else {
+    let isPrime = true;
+    for (let jakaja = 2; jakaja <= Math.sqrt(num); jakaja++) { //Math.sqrt laskee ohjelman laskentatehoa, ei tarvitse käydä niin monta lukua läpi.
+        if (num % jakaja === 0) {
+        isPrime = false;
+        break;
+        }
+    }
+
+if (isPrime) {
+    document.querySelector("#answer").innerHTML = "Given number " +num + " is a prime number." 
+} else {
+    document.querySelector("#answer").innerHTML = "Given number " +num + " is not a prime number."
+}
+}
